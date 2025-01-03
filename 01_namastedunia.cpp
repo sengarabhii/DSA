c7b3586bd6348eca176abe45e722335fa9d9441a
Abhishek Sengar <sengarabhishek91@gmail.com>
2025-01-03 20:56:02 +0530

    y

---
+++ 01_namastedunia.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*cout << "I am Abhishek Sengar" ;
+    cout << "\n";
+    cout << "Hello" <<endl;
+    cout << "Namaste duniya";
+    cout << "Size of int is " << sizeof(int) <<endl;
+    cout << "Size of  is char " << sizeof(char) <<endl;
+    cout << "Size of float is " << sizeof(float) <<endl;
+    cout << "Size of double is " << sizeof(double) <<endl;
+    int a = 5;
+    cout << a << endl;
+    char b = 'A';
+    cout << b << endl;
+    float c = 1.22;
+    cout << c << endl;
+    double d = 1.2222222;
+    cout << d << endl;
+    int e = sizeof(a);
+    cout << e <<endl;
+    int f = sizeof(b);
+    cout << f <<endl;
+    int g = sizeof(c);
+    cout << g <<endl;
+    int h = sizeof(d);
+    cout << h <<endl;*/
+    int a = 5;
+    int b = 6;
+    bool p = (a==b);
+    cout << p <<endl;
+    bool q = (a>b);
+    cout << q << endl;
+    int c = 21;
+    cout << !c << endl ;
+    int d = 0;
+    cout << !d;
+    
+    
+}
